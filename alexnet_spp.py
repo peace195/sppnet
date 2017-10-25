521d418e5ab02652fed8c0d5d1f3dd90779f6348
peace195 <magic10995@gmail.com>
2017-10-25 23:18:25 +0700

    fix spp function using padding and ceil() function for pooling stride parameter

--- alexnet_spp.py
+++ alexnet_spp.py
@@ -117,14 +117,13 @@ def spatial_pyramid_pool(previous_conv, num_sample, previous_conv_size, out_pool
     '''
 
     for i in range(len(out_pool_size)):
-        h_strd = previous_conv_size[0] / out_pool_size[i]
-        w_strd = previous_conv_size[1] / out_pool_size[i]
-        h_wid = previous_conv_size[0] - h_strd * out_pool_size[i] + 1
-        w_wid = previous_conv_size[1] - w_strd * out_pool_size[i] + 1
+        h_strd = math.ceil(previous_conv_size[0] / out_pool_size[i])
+        w_strd = math.ceil(previous_conv_size[1] / out_pool_size[i])
+        
         max_pool = tf.nn.max_pool(previous_conv,
-                                   ksize=[1,h_wid,w_wid, 1],
+                                   ksize=[1,h_strd, w_strd, 1],
                                    strides=[1,h_strd, w_strd,1],
-                                   padding='VALID')
+                                   padding='SAME')
         if (i == 0):
             spp = tf.reshape(max_pool, [num_sample, -1])
         else:
